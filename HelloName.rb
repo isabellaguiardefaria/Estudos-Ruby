fc7887d5312958f374e29ad39ab8ab63383de712
Isabella Guimarães Aguiar de Faria <isabellaguiardefaria@gmai.com>
2021-04-28 16:24:15 -0300

    Hello

---
+++ HelloName.rb
@@ -0,0 +1,9 @@
+# saída de dado --> print não pula linha
+print 'Digite seu nome: '
+ 
+# Recebendo uma entrada do teclado
+name = gets.chomp
+ 
+# saída utilizando puts --> pula linha
+# use códigos ruby dentro de uma string com #{code}
+puts "Hello #{name}!"
